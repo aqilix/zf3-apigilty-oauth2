d9c05ad95365b84a422d0809d73de8ff2af38bc0
Dolly Aswin Harahap <dolly.aswin@gmail.com>
2016-08-24 06:20:03 +0000

    Add ZfrCors For Development purpose Enable ZfrCors Module

--- module/Application/src/Module.php
+++ module/Application/src/Module.php
@@ -6,8 +6,19 @@
 
 namespace Application;
 
+use Zend\Uri\UriFactory;
+use Zend\Mvc\MvcEvent;
+
 class Module
 {
+    public function onBootstrap(MvcEvent $mvcEvent)
+    {
+        UriFactory::registerScheme('chrome-extension', 'Zend\Uri\Uri'); // add chrome-extension for API Client
+        $serviceManager = $mvcEvent->getApplication()->getServiceManager();
+        $eventManager   = $mvcEvent->getApplication()->getEventManager();
+        $sharedEventManager = $eventManager->getSharedManager();
+    }
+
     public function getConfig()
     {
         return include __DIR__ . '/../config/module.config.php';
