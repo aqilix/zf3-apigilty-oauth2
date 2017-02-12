33f71f1a86a286f1c6c5c5af133ea287d1fd5e99
Dolly Aswin <dolly.aswin@gmail.com>
2017-02-12 10:34:16 +0700

    Add logger configuration

--- module/Aqilix/config/module.config.php
+++ module/Aqilix/config/module.config.php
@@ -1,15 +1,31 @@
 <?php
+use Zend\Log\Logger;
+
 return [
-    'service_manager' => [
-        'invokables' => [
-            'oauth2.accessToken' => 'Aqilix\OAuth2\ResponseType\AccessToken'
+    "service_manager" => [
+        "invokables" => [
+            "oauth2.accessToken" => "Aqilix\OAuth2\ResponseType\AccessToken"
         ],
-        'factories'  => [
-            'Aqilix\Service\Mail' => \Aqilix\Service\Mail\MailgunAppFactory::class,
-            'Aqilix\Service\PhpProcessBuilder' => \Aqilix\Service\PhpProcessFactory::class
+        "factories"  => [
+            "Aqilix\Service\Mail" => \Aqilix\Service\Mail\MailgunAppFactory::class,
+            "Aqilix\Service\PhpProcessBuilder" => \Aqilix\Service\PhpProcessFactory::class
         ],
-        'abstract_factories' => [
-            'Aqilix\OAuth2\Mapper\AbstractMapperFactory'
+        "abstract_factories" => [
+            "Aqilix\OAuth2\Mapper\AbstractMapperFactory",
+            "Zend\Log\LoggerAbstractServiceFactory",
+        ]
+    ],
+    "log" => [
+        "logger_default" => [
+            "writers" => [
+                [
+                    "name" => "stream",
+                    "priority" => Logger::DEBUG,
+                    "options" => [
+                        'stream' => 'data/log/system.log',
+                    ]
+                ]
+            ]
         ]
     ]
 ];
